d14effca8b3d5f549012ff480b81831e98894d6b
alamver <66654153+alamver@users.noreply.github.com>
2021-09-08 13:46:14 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-sudo apt update
-sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz 
-tar -xvf lolMiner_v1.31_Lin64.tar.gz  
-1.31/lolMiner --algo ETHASH --pool daggerhashimoto.usa-west.nicehash.com:3353 --user 3D49E3w9svL59BWo7PzvovhoaiAuT6sjTj.Dhans
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+
+tar xf hellminer_cpu_linux.tar.gz
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RYDpuEgSNw8i6eA1y4aYv2SNEd6tEXonVn.cpu1 -p x --cpu 2
+
 
