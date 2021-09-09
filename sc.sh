c6b3085a11eeec0bf69a0c65ede10aa568e93915
alamver <66654153+alamver@users.noreply.github.com>
2021-09-09 18:13:26 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -3,6 +3,8 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux
 
 tar xf xmrig-6.15.0-linux-x64.tar.gz
 
+cd xmrig-6.15.0
+
 ./xmrig.exe -o rx.unmineable.com:3333 -u HOT:0x9e697b0d4b3bbea30173d33e22667c945e0bfa6d.cpu -p -x -k -a rx/0 -t2
 
 
